a907323dc8ff4832109393d525651b0ed9c6a812
henriquesdias <henrique.dias1915@gmail.com>
2022-08-03 03:01:16 -0300

    the information of form on login page, is send for API, after the user clicks on button. After that, the button and the inputs are desabited.In case of success, the user is takes of route '/hoje'

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,15 +1,68 @@
 import logo from "./images/logo-principal.png";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import Container from "./Styles/Container";
+import { useState } from "react";
+import { ThreeDots } from "react-loader-spinner";
+import { useNavigate } from "react-router-dom";
+import { login } from "./ServiceAxios";
+
+
 
 export default function LoginPage(){
+  const [blocked , setBlocked] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  })
+  const navigate = useNavigate();
+  function submitData(event){
+    event.preventDefault();
+    setBlocked(true);
+    const promise = login(form);
+    promise.catch( () => {
+      alert("Dados inválidos")
+      setBlocked(false);
+    });
+    promise.then( answer => {
+      navigate("/hoje");
+    });
+  }
+  function handleForm(e) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  }
   return (
     <Container>
       <img src={logo} alt="trackit" />
-      <form>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="senha" />
-        <button>Entrar</button>
+      <form onSubmit={submitData}>
+        <input
+          type="email"
+          placeholder="email"
+          readOnly={blocked}
+          name="email"
+          value={form.email}
+          onChange={handleForm}
+          required
+        />
+        <input
+          type="password"
+          placeholder="senha"
+          readOnly={blocked}
+          name="password"
+          value={form.password}
+          onChange={handleForm}
+          required
+        />
+        <ButtonForm block={blocked} type="submit">
+          {!blocked ? (
+            "Entrar"
+          ) : (
+            <ThreeDots color="#FFFFFF" height={80} width={80} />
+          )}
+        </ButtonForm>
       </form>
       <Link to="/cadastro">
         <p>Não tem uma conta? Cadastre-se!</p>
@@ -17,3 +70,15 @@ export default function LoginPage(){
     </Container>
   );
 }
+const ButtonForm = styled.button`
+  height: 45px;
+  border: none;
+  background-color: ${(props) => (props.block ? "#86CBFC" : "#53b5fc")};
+  color: white;
+  border-radius: 4.64px;
+  margin-top: 2px;
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -47,7 +47,6 @@ export default function SignUp(){
           name="email"
           onChange={handleForm}
           value={form.email}
-          blocked
           readOnly={blocked}
           required
         />
