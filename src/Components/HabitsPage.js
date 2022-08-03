9cd7d820ede4e8b5d9112090181d205bd21f27a5
henriquesdias <henrique.dias1915@gmail.com>
2022-08-03 00:25:04 -0300

    layout of first page of route 'habitos' was finished

--- src/Components/HabitsPage.js
+++ src/Components/HabitsPage.js
@@ -6,13 +6,49 @@ export default function HabitsPage(){
   return (
     <>
       <Header />
-      <MainStyle></MainStyle>
+      <BackGroundPage>
+        <MyHabits>
+          <span>Meus hábitos</span>
+          <AddHabit>+</AddHabit>
+        </MyHabits>
+        <p>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</p>
+      </BackGroundPage>
       <Footer />
     </>
   );
 }
-const MainStyle = styled.main`
+const BackGroundPage = styled.main`
   background-color: #f2f2f2;
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
+  padding-top: 98px;
+  font-family: 'Lexend Deca', sans-serif;
+  p{
+    width: 80%;
+    margin: 28px auto 0 auto;
+    font-size: 18px;
+    color: #666666;
+  }
 `;
+const MyHabits = styled.div`
+  margin: 0 auto 0 auto;
+  width: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  span{
+    font-size: 23px;
+    color: #126BA5;
+  }
+`
+const AddHabit = styled.div`
+  background-color: #52B6FF;
+  width: 40px;
+  height: 35px;
+  border-radius: 4.6px;
+  font-size: 27px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
