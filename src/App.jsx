a161f3c6d0c2df174e39039de96a0320e3c9c447
henriquesdias <henrique.dias1915@gmail.com>
2023-03-03 19:33:27 -0300

    feat: Implements realoading

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ import UserContext from "./context/UserContext";
 export default function App() {
   const [user, setUser] = useState(null);
   const [percentageOfHabits, setPercentageOfHabits] = useState(0);
+  const userLogin = JSON.parse(localStorage.getItem("userLogin"));
+  if (userLogin !== null && user === null) {
+    setUser(userLogin);
+  }
 
   return (
     <BrowserRouter>
